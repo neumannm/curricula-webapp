5d6ab2a2f3b78e98a21df5626d7535df308f27ae
Mandy Neumann <mandy.neumann@th-koeln.de>
2019-03-19 11:46:37 +0100

    Update changelog to change ects datatype in db

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1,7 +1,6 @@
 databaseChangeLog = {
-    changeSet(author: "mandy (generated)", id: "1548687418332-1") {
-        addColumn(tableName: "programmes") {
-            column(name: "summary", type: "text")
-        }
-    }
+	changeSet(author: 'mandy', id: 'modules.ects') {
+		comment {'Change data type of ects column' }
+		modifyDataType(tableName: 'modules', columnName: 'ects', newDataType: 'float(10)')
+	}
 }
